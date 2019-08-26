fa07e2eca36a5462c6358f58f7a11fb8687d1d8f
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-25 21:56:19 -0500

    First try at session management

--- database_connection.php
+++ database_connection.php
@@ -1,1 +1,11 @@
 <?php
+
+$host = 'localhost';
+$db = 'invsys';
+$username = 'postgres';
+$password = '42IsTheAnswer';
+
+$connect = new PDO('pgsql:dbname=$db;host=$host', $username, $password);
+session_start();
+
+?>
