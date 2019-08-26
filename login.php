51f603f7c3ccb385ed0dc57d0a3eedcd59e4376b
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-25 21:23:30 -0500

    finished modeling login thingies

--- login.php
+++ login.php
@@ -10,8 +10,32 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
     </head>
     <body>
+        <br />
+        <div class="container">
+            <h2 align="center"> UpVent Management System </h2>
+        </div>
+        <br />
 
-
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Login
+            </div>
+            <div class="panel-body">
+                <form method="post">
+                    <div class="form-group">
+                        <label>Correo Electrónico</label>
+                        <input type="text" name="user_email" class="form-control" required/>
+                    </div>
+                    <div class="form-group">
+                        <label>Contraseña</label>
+                        <input type="password" name="user_password" class="form-control" required />
+                    </div>
+                    <div class="form-group">
+                        <input type="submit" name="login" value="Login" class="btn btn-info" />
+                    </div>
+                </form>
+            </div>
+        </div>
        <!-- JQuery -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <!-- Bootstrap tooltips -->
