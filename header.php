60f300ab37be0a3cd4a4c854f0a42effdd9ccf46
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-08-26 20:35:45 -0500

    Added basic header functionality

--- header.php
+++ header.php
@@ -11,6 +11,12 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.8/css/mdb.min.css" rel="stylesheet">
     </head>
     <body>
+        <br />
+        <div class="container">
+            <h2 align="center">UpVent Management System</h2>
+
+
+        </div>
 
         <!-- JQuery -->
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
